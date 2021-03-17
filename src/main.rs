4b3f6a272daa050ec788efce0ea1d96b2dd9c58a
Joakim Soderlund <joakim.soderlund@gmail.com>
2021-03-17 15:48:00 +0100

    Add initial index route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::*;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hellopaca, World!"
+}
+
+#[launch]
+fn initialize() -> Rocket {
+    ignite().mount("/", routes![index])
 }
